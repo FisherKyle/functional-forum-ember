c15154d0e6613632b73dd3d7a3cce11478ec42e7
kyle <kyle@kylefisher.com>
2016-11-12 04:19:50 -0500

    add the ability to toggle the display of a question's details.

---
+++ app/components/query-tile.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+  export default Ember.Component.extend({
+
+    detailsVisible: false,
+    actions: {
+
+      showContent: function() {
+        this.set('detailsVisible', true);
+      },
+
+      hideContent: function() {
+       this.set('detailsVisible', false);
+     }
+   }
+ });
