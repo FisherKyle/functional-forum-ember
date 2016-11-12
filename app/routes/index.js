3560034ac9f89b115381974b24d34ce6b98189c1
kyle <kyle@kylefisher.com>
2016-11-12 01:04:58 -0500

    create an index route, begin the import of mock json data into the project

---
+++ app/routes/index.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+var mockQ = [{
+
+    "author": "Dividad Van Tomlinpot",
+    "query": "Have you or have you ever known someone whose fallen victim to diphtheria on the oregon trail?",
+    "notes": "I've already tried epsom salts."
+  }, {
+
+    "author": "Sharmy Proverbencaux",
+    "query": "Might someone offer the means by which I might saveth this poor scorch-ed casserole?",
+    "notes": "If there exists a way in which my pastas might not be squanderethed, I'd be ever-so gratefuleth!"
+  }, {
+
+    "author": "Vincenzo Armani Marino Badoosh",
+    "query": "Hey. How you doin'?",
+    "notes": "Awlriight.. submit. Eyyyy..."
+  }],
+
+export default Ember.Route.extend({
+});
