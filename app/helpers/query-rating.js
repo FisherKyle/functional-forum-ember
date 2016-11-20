e50619487ac205d150abefa90763384da5d5350a
kyle <kyle@kylefisher.com>
2016-11-20 13:20:23 -0500

    generate and link a rating helper

---
+++ app/helpers/query-rating.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export function queryRating(params/*, hash*/) {
+
+  var query = params[0];
+
+  if(query.get('replies').get('length') >= 5) {
+
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire">trending!</span>');
+
+  }
+}
+
+export default Ember.Helper.helper(queryRating);
---
+++ tests/unit/helpers/query-rating-test.js
@@ -0,0 +1,12 @@
+
+import { queryRating } from 'ember-board/helpers/query-rating';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | query rating');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = queryRating([42]);
+  assert.ok(result);
+});
+
