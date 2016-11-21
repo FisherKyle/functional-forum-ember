367b00b95df5381cb55fe403156b07e4e243a7f4
kyle <kyle@kylefisher.com>
2016-11-21 07:56:33 -0500

    create a service for sorting and displaying top ranked user answers as well a component for displaying the results

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  author-rating: Ember.computed('reply.author', 'reply.rating', function() {
-    return " Rating for " + this.get('reply.author') + "'s answer : " +  this.get('reply.rating');
+  details: Ember.computed('reply.author', 'reply.rating', function() {
+    return this.get('reply.author') + ' // ' + this.get('reply.rating');
   }),
 
   actions: {
